96636bcf143512706303584cb066eb496f481b30
Raghad Al Musawi <raghad3@pdx.edu>
2024-10-14 18:25:12 -0700

    Palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,24 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+let check = document.getElementById("check");
+check.innerHTML = "";
 
-elem.addEventListener('input', handleInput);
+let handleInput = () => {
+  const number = elem.value;
+  let numberString = number.toString();
+  let numberRev = numberString.split("").reverse().join("");
+  if (number < 0) {
+    check.innerHTML = "Not Vaild! Please enter a positve number";
+    check.style.color = "red";
+    return;
+  } else {
+    if (numberString === numberRev) {
+      check.innerHTML = "It is plindrome";
+      check.style.color = "blue";
+    } else {
+      check.innerHTML = "It is not a plindrome";
+      check.style.color = "gray";
+    }
+  }
+  return;
+};
+elem.addEventListener("input", handleInput);
