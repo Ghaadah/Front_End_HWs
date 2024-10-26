5d11fb451d8d6dfbc041dd4f1d7323d67754f457
Raghad <91997030+Ghaadah@users.noreply.github.com>
2024-10-26 01:18:54 -0700

    Hw1 (#1)
    * 01-flexbox
    
    * Palindrome
    
    * Complete hw1
    
    * updated hw1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,25 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+let handleInput = () => {
+  const number = elem.value;
+  let numberString = number.toString();
+  let numberRev = numberString.split("").reverse().join("");
+  console.log(number, numberString, numberRev);
+
+  let check = document.getElementById("check");
+  check.innerText = "";
+  if (number < 0) {
+    check.innerText = "Not Vaild! Please enter a positve number";
+    check.style.color = "#FF0000";
+  } else {
+    if (numberString === numberRev) {
+      check.innerText = "Yes, This is a plindrome!";
+      check.style.color = "#556B2F";
+    } else {
+      check.innerText = "No, Try again.";
+      check.style.color = "#FF0000";
+    }
+  }
+  return;
+};
+elem.addEventListener("input", handleInput);
